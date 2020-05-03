31cac196f869db7711e4d6d26227f2638e0c1bff
Dominik Suesser <dominik.suesser@student.uni-augsburg.de>
2020-05-03 20:14:47 +0200

    Abgabe Blatt03 Aufgabe 04

--- Abgaben/Einzelabgaben/Suesser/Uebungsprojekt/Uebungsprojekt/Controllers/HomeController.cs
+++ Abgaben/Einzelabgaben/Suesser/Uebungsprojekt/Uebungsprojekt/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Uebungsprojekt.Controllers
             return View();
         }
 
+        public IActionResult Booking()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- Abgaben/Einzelabgaben/Suesser/Uebungsprojekt/Uebungsprojekt/Models/Booking.cs
+++ Abgaben/Einzelabgaben/Suesser/Uebungsprojekt/Uebungsprojekt/Models/Booking.cs
@@ -3,8 +3,14 @@ namespace Uebungsprojekt.Models
 {
     public class Booking
     {
+        public int charge { get; set; }
+        public int needed_distance { get; set; }
+        public DateTime start_time { get; set; }
+        public DateTime end_time { get; set; }
+
         public Booking()
-        {
+        { 
+
         }
     }
 }
