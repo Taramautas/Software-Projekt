a5b6b47c7dab0546fca43f15f4268c5ef94fd624
Benedikt Volger <benedikt.volger@student.uni-augsburg.de>
2020-05-07 14:21:06 +0200

    Abgabe Blatt3

---
+++ Abgaben/Einzelabgaben/Volger/Blatt3/WebApplication2/WebApplication2/Models/Booking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Booking
+    {
+        public int SoC { get; set; }
+
+        public int reqDistance { get; set; }
+
+        public DateTime start { get; set; }
+
+        public DateTime end { get; set; }
+        
+        public Booking()
+        {
+
+        }
+    }
+}
