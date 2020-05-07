4854e1abc111415a08ca901cbb59a86f2428d506
Marcin Kuhnert <martink1998@googlemail.com>
2020-05-07 12:09:27 +0200

    dd

---
+++ Abgaben/Einzelabgaben/Suesser/Uebungsprojekt/Uebungsprojekt/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Uebungsprojekt.Models
+{
+    public class Booking
+    {
+        public int charge { get; set; }
+        public int needed_distance { get; set; }
+        public DateTime start_time { get; set; }
+        public DateTime end_time { get; set; }
+
+        public Booking()
+        { 
+
+        }
+    }
+}
