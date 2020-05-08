cd8aa5ee94023a98ebd7fb4dc26acd782d5bf4c7
Marcin Kuhnert <martink1998@googlemail.com>
2020-05-08 21:18:27 +0200

    letzte Hausaufgabe

---
+++ Abgaben/Einzelabgaben/Kuhnert/Visual Studio/WebApplication1/Controllers/BookingController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BookingController : Controller
+    {
+        List<Booking> bookings = new List<Booking>();
+
+        public BookingController()
+        {
+            bookings.Add(new Booking(20, 200, new DateTime(2020, 10, 10, 10, 10, 00), new DateTime(2020, 10, 10, 20, 0, 0)));
+            bookings.Add(new Booking(20, 200, new DateTime(2020, 10, 10, 10, 10, 00), new DateTime(2020, 10, 10, 20, 0, 0)));
+            bookings.Add(new Booking(20, 200, new DateTime(2020, 10, 10, 10, 10, 00), new DateTime(2020, 10, 10, 20, 0, 0)));
+            bookings.Add(new Booking(20, 200, new DateTime(2020, 10, 10, 10, 10, 00), new DateTime(2020, 10, 10, 20, 0, 0)));
+            bookings.Add(new Booking(20, 200, new DateTime(2020, 10, 10, 10, 10, 00), new DateTime(2020, 10, 10, 20, 0, 0)));
+            bookings.Add(new Booking(20, 200, new DateTime(2020, 10, 10, 10, 10, 00), new DateTime(2020, 10, 10, 20, 0, 0)));
+            bookings.Add(new Booking(20, 200, new DateTime(2020, 10, 10, 10, 10, 00), new DateTime(2020, 10, 10, 20, 0, 0)));
+            bookings.Add(new Booking(20, 200, new DateTime(2020, 10, 10, 10, 10, 00), new DateTime(2020, 10, 10, 20, 0, 0)));
+            bookings.Add(new Booking(20, 200, new DateTime(2020, 10, 10, 10, 10, 00), new DateTime(2020, 10, 10, 20, 0, 0)));
+            bookings.Add(new Booking(20, 200, new DateTime(2020, 10, 10, 10, 10, 00), new DateTime(2020, 10, 10, 20, 0, 0)));
+        }
+
+        
+        public IActionResult Index()
+        {
+            return View(bookings);
+        }
+    }
+}
