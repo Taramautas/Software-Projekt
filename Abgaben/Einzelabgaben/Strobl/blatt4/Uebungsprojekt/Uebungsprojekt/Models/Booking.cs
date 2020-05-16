cba6a171ed9cba2693ddb5537e760bc00b506afa
stroblma <marcel.strobl@student.uni-augsburg.de>
2020-05-16 19:01:57 +0200

    abgabe blatt4 marcel

---
+++ Abgaben/Einzelabgaben/Strobl/blatt4/Uebungsprojekt/Uebungsprojekt/Models/Booking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace Uebungsprojekt.Models
+{
+    public class Booking
+    {
+        [Required]
+        [Range(0, 100)]
+        public int charge { get; set; }
+
+        [Required]
+        [Range(1, 1000)]
+        public int needed_distance { get; set; }
+
+        [Required]
+        public DateTime start_time { get; set; }
+
+        [Required]
+        public DateTime end_time { get; set; }
+
+        public Booking()
+        { 
+
+        }
+    }
+}
