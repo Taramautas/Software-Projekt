fec3eec19dff85681ba98970df9edb871da0a36b
Dominik Suesser <dominik.suesser@student.uni-augsburg.de>
2020-05-20 15:05:02 +0200

    Einzelabgabe Blatt 05 Aufgabe 04 - Auf Basis der Musterlösung

---
+++ Abgaben/Einzelabgaben/Suesser/Blatt04/Models/Booking.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Uebungsprojekt.Models
+{
+    /// <summary>
+    /// Model representing one specific booking 
+    /// </summary>
+    public class Booking
+    {
+        /// <summary>Current State of Charge</summary>
+        [Required(ErrorMessage = "Bitte geben sie den Ladezustand ihres Autos an.")]
+        [Range(0, 100)]
+        public int Charge { get; set; }
+
+        /// <summary>Distance needed before next charging</summary>
+        [Required(ErrorMessage = "Bitte geben sie die benötigte Fahrtstrecke an.")]
+        [Range(1, 1000)]
+        public int Needed_distance { get; set; }
+
+        /// <summary>Preferred start datetime</summary>
+        [Required(ErrorMessage = "Bitte geben sie die Startzeit des Ladens an.")]
+        public DateTime Start_time { get; set; }
+
+        /// <summary>Preferred end datetime</summary>
+        [Required(ErrorMessage = "Bitte geben sie die Endzeit des Ladens an.")]
+        public DateTime End_time { get; set; }
+
+        /// <summary>
+        /// Empty constructor of booking model
+        /// </summary>
+        public Booking()
+        { 
+
+        }
+
+        public Steckertyp s_type { get; set; }
+
+        public enum Steckertyp
+        {
+            Schuko_Socket,
+            Type_1_Plug,
+            Type_2_Plug,
+            CHAdeMO_Plug,
+            Tesla_Supercharger,
+            CCS_Combo_2_Plug
+        }
+    }
+}
