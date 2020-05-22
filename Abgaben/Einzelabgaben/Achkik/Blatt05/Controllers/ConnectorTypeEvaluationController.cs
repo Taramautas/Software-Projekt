0c895b94aea5fe57c483b8a753c52784c1bfc4d0
radiachk <Ra21022000>
2020-05-22 22:05:48 +0200

    Radi Blatt05

---
+++ Abgaben/Einzelabgaben/Achkik/Blatt05/Controllers/ConnectorTypeEvaluationController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using Uebungsprojekt.Models;
+using Uebungsprojekt.ViewModel;
+
+namespace Uebungsprojekt.Controllers
+{
+    public class ConnectorTypeEvaluationController : Controller
+    {
+        private IMemoryCache _cache;
+
+        /// <summary>
+        /// Constructor of controller.
+        /// </summary>
+        /// <param name="memoryCache">IMemoryCache object for initializing the memory cache</param>
+        public ConnectorTypeEvaluationController(IMemoryCache memoryCache)
+        {
+            _cache = memoryCache;
+        }
+
+        /// <summary>
+        /// Retuns a list, displaying the proportion of each connector type in respect of the total amount of bookings
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            // List for passing to the view
+            List<ConnectorTypeEvaluationViewModel> connector_types = new List<ConnectorTypeEvaluationViewModel>();
+            // Dict for counting the proportions of the different connector types
+            Dictionary<Booking.ConnectorType, int> proportions = new Dictionary<Booking.ConnectorType, int>();
+            // Counting the total amount of bookings
+            int total_count = 0;
+            
+            // Count occurrences of connector types in all bookings
+            if (_cache.TryGetValue("CreateBooking", out List<Booking> bookings))
+            {
+                foreach (Booking b in bookings)
+                {
+                    if (!proportions.ContainsKey(b.Connector_Type))
+                    {
+                        proportions[b.Connector_Type] = 1;
+                    }
+                    else
+                    {
+                        proportions[b.Connector_Type] += 1;
+                    }
+                    total_count += 1;
+                }
+                // Calculate the percentages and add ConnectorTypeEvaluation objects to view accordingly
+                foreach (KeyValuePair<Booking.ConnectorType, int> entry in proportions)
+                {
+                    double proportion = entry.Value / (double)total_count * 100;
+                    connector_types.Add(
+                        new ConnectorTypeEvaluationViewModel(entry.Key, proportion)
+                        );
+                }
+            }
+            return View(connector_types);
+        }
+    }
+}
---
+++ Abgaben/Einzelabgaben/Achkik/Blatt05/Models/Booking.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Uebungsprojekt.Models
+{
+    /// <summary>
+    /// Model representing one specific booking 
+    /// </summary>
+    public class Booking : INotifyPropertyChanged
+    {
+        /// <summary> Event handler for property changes </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>All possible connector types</summary>
+        public enum ConnectorType
+        {
+            [Display(Name = "Schuko Socket")]
+            Schuko_Socket,
+            [Display(Name = "Type 1 Plug")]
+            Type_1_Plug,
+            [Display(Name = "Type 2 Plug")]
+            Type_2_Plug,
+            [Display(Name = "CHAdeMO Plug")]
+            CHAdeMO_Plug,
+            [Display(Name = "Tesla Supercharger")]
+            Tesla_Supercharger,
+            [Display(Name = "CCS Combo 2 Plug")]
+            CCS_Combo_2_Plug
+        };
+
+        /// <summary>Current State of Charge</summary>
+        [Required(ErrorMessage = "Please specify the current state of charge.")]
+        [Range(0, 100)]
+        public int Charge { get; set; }
+
+        /// <summary>Distance needed before next charging</summary>
+        [Required(ErrorMessage = "Please specify the distance needed.")]
+        [Range(1, 1000)]
+        public int Needed_distance { get; set; }
+
+        /// <summary>Preferred start datetime</summary>
+        [Required(ErrorMessage = "Please specify the wanted start time.")]
+        public DateTime Start_time { get; set; }
+
+        /// <summary>Preferred end datetime</summary>
+        [Required(ErrorMessage = "Please specify the wanted end time.")]
+        public DateTime End_time { get; set; }
+
+        /// <summary>Connector type for charging</summary>
+        [Required(ErrorMessage = "Please select at least one of the plug types.")]
+        public ConnectorType Connector_Type { get; set; }
+
+        /// <summary>
+        /// Empty constructor of booking model
+        /// </summary>
+        public Booking()
+        { 
+
+        }
+        
+        /// <summary>
+        /// Implement event handling in order to be observable for ViewModel
+        /// </summary>
+        /// <param name="property"> The property that has changed</param>
+        private void RaisePropertyChanged(string property) {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            } 
+        } 
+    }
+}
