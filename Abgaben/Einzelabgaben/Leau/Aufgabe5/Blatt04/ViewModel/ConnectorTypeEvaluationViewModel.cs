048b6ca7ab1f04a542ed34795d5410e88ebbf995
Elena Leau <elena.leau@student.uni-augsburg.de>
2020-05-22 22:39:10 +0200

    Aufgabe5

---
+++ Abgaben/Einzelabgaben/Leau/Aufgabe5/Blatt04/ViewModel/ConnectorTypeEvaluationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Uebungsprojekt.Models.Booking;
+
+namespace Uebungsprojekt.ViewModel
+{
+    public class ConnectorTypeEvaluationViewModel
+    {
+        public ConnectorType connectorType { get; set; }
+
+        public int percentage { get; set; }
+
+        public ConnectorTypeEvaluationViewModel() 
+        {
+
+        }
+    }
+}
