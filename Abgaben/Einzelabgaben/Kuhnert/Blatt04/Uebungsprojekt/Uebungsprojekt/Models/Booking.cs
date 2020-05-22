aba00ca3a2e3c25eb26297e7d001e635d3dd5b2e
Marcin Kuhnert <martink1998@googlemail.com>
2020-05-22 22:10:12 +0200

    Hallo

--- Abgaben/Einzelabgaben/Kuhnert/Blatt04/Uebungsprojekt/Uebungsprojekt/Models/Booking.cs
+++ Abgaben/Einzelabgaben/Kuhnert/Blatt04/Uebungsprojekt/Uebungsprojekt/Models/Booking.cs
@@ -4,6 +4,15 @@ namespace Uebungsprojekt.Models
 {
     public class Booking
     {
+        [Flags]
+        public enum ConnectorType
+        {
+            Schuko_Socket, Type_1_Plug, Type_2_Plug, CHAdeMO_Plug, Tesla_Supercharger, CCS_Combo_2_Plug
+
+
+        }
+        [Required]
+        public String connectorType { get; set; }
         [Required]
         [Range(0, 100)]
         public int charge { get; set; }
---
+++ Abgaben/Einzelabgaben/Kuhnert/Blatt05/Controllers/ConnectorTypeEvaluationController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Uebungsprojekt.Models;
+using Microsoft.Extensions.Caching.Memory;
+using Uebungsprojekt.ViewModel;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Uebungsprojekt.Controllers
+{
+    public class ConnectorTypeEvaluationController : Controller
+    {
+        List<ConnectorTypeEvaluationViewModel> evaluationList;
+        List<Booking> bookings = new List<Booking>();
+        private IMemoryCache _cache;
+
+
+        /// <summary>
+        /// Controller Constructer which using memoryCache
+        /// </summary>
+        /// <param name="memoryCache">IMemoryCache object for intializing the memory cache</param>
+        public ConnectorTypeEvaluationController(IMemoryCache memoryCache)
+        {
+            _cache = memoryCache;
+
+        }
+
+
+        /// <summary>
+        /// Create the View of the Evaluation List
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            updateEvaluationList();
+            return View(evaluationList);
+        }
+
+        /// <summary>
+        /// Updating the Evaluation List of Connector Types
+        /// </summary>
+
+        private void updateEvaluationList()
+        {
+            evaluationList = new List<ConnectorTypeEvaluationViewModel>();
+            foreach (var type in Enum.GetValues(typeof(Booking.ConnectorType)).Cast<Booking.ConnectorType>())
+            {
+                evaluationList.Add(new ConnectorTypeEvaluationViewModel
+                {
+                    connectorType = type,
+                    Percentage = 0.0
+                });
+            }
+            bookings = (List<Booking>)_cache.Get("CreateBooking");
+
+            if (bookings == null) return;
+            if (bookings.Count == 0) return;
+
+            foreach (var booking in bookings)
+            {
+                foreach (var eval in evaluationList)
+                {
+                    if (booking.connectorType.Equals(eval.connectorType)) eval.Percentage += 1;
+                }
+            }
+
+            foreach (var eval in evaluationList)
+            {
+                eval.Percentage = Math.Round((eval.Percentage / bookings.Count) * 100);
+            }
+        }
+    }
+}
