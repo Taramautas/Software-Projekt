4415768a52ce632bd6ca05b4ad0786937975e44e
Dominik Suesser <dominik.suesser@student.uni-augsburg.de>
2020-06-10 18:40:55 +0200

    ChangeErrors

---
+++ Abgaben/Einzelabgaben/Leau/Aufgabe4/Uebungsprojekt/Uebungsprojekt/Models/Booking 2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Uebungsprojekt.Models
+{
+    public class Booking
+    {
+        [Range(0,100)]
+        [Required]
+        public int charge { get; set; }
+        [Required]
+        [Range(1, 1000)]
+        public int needed_distance { get; set; }
+        [Required]
+        public DateTime start_time { get; set; }
+        [Required]
+        public DateTime end_time { get; set; }
+        
+        public Booking()
+        { 
+
+        }
+    }
+}
---
+++ Abgaben/Einzelabgaben/Strobl/blatt3/blatt3aufgabe2/blatt3aufgabe2/Models/Booking 2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blatt3aufgabe2.Models
+{
+    public class Booking
+    {
+        public int ladestand { get; set; }
+        public int fahrstrecke { get; set; }
+        public DateTime startzeit { get; set; }
+        public DateTime endzeit { get; set; }
+        
+        public Booking()
+        {
+
+        }
+    }
+}
