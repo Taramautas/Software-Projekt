9e81de96a5100ff3222b2cdb67b06a378e51672e
Dominik Suesser <dominik.suesser@student.uni-augsburg.de>
2020-06-11 20:07:04 +0200

    Added UserDashboardController

---
+++ Uebungsprojekt/Controllers/UserDashboardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+
+namespace Uebungsprojekt.Controllers
+{
+    public class UserDashboardController : Controller
+    {
+        public UserDashboardController()
+        {
+            //TODO
+        }
+
+        //TODO Evaluate if this Class is the right place for the notifyUserOnChargingWindow() method declared in issue #22 
+    }
+}
