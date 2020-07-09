cb954f2061d676dad8c293737cee523c3c6bc0b8
Marcin Kuhnert <martink1998@googlemail.com>
2020-07-09 12:30:10 +0200

    Import überarbeitet

---
+++ Uebungsprojekt/Models/ChargingColumn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uebungsprojekt.Models
+{
+    public class ChargingColumn
+    {
+        public int Id { get; set; }
+
+        public Boolean Busy { get; set; }
+
+        public string Manufacturer_name { get; set; }
+
+        public Dictionary<ConnectorType, ConnectorType> Connectors { get; set; }
+
+        public Boolean Emergency_reserve { get; set; }
+
+        public int Max_concurrent_charging { get; set; }
+
+        public ChargingColumn()
+        {
+
+        }
+    }
+}
